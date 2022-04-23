9008866aa59b7d2b1554ed82f1335fb73d81b182
haypritam <haypritam@gmail.com>
2022-04-23 18:35:07 +0530

    game completed Scoreboard due

--- day 23/main.py
+++ day 23/main.py
@@ -8,9 +8,25 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player=Player()
+car_manager=CarManager()
+
+screen.listen()
+screen.onkey(player.go_up,"Up")
+
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
 
-screen.exitonclick()ea
+    car_manager.create_car()
+    car_manager.move_cars()
+    for car in car_manager.all_cars:
+        if player.distance(car)<20:
+            game_is_on = False
+    if player.ycor()==280:
+        player.re()
+        car_manager.STARTING_MOVE_DISTANCE+=car_manager.MOVE_INCREMENT
+
+screen.exitonclick()
--- day 23/player.py
+++ day 23/player.py
@@ -1,7 +1,18 @@
+from turtle import Screen,Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.pu()
+        self.goto(STARTING_POSITION)
+        self.lt(90)
+    def go_up(self):
+        self.fd(MOVE_DISTANCE)
+    def re(self):
+        self.goto(0, -280)
