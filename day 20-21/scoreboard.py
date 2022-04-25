c2be7ad577a579a00eadfc435a8d6c8f93f32ac3
haypritam <haypritam@gmail.com>
2022-04-25 19:52:50 +0530

    adding high score

--- day 20-21/scoreboard.py
+++ day 20-21/scoreboard.py
@@ -5,6 +5,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score=0
+        self.high_score=0
         self.color("white")
         self.pu()
         self.goto(0,270)
@@ -12,13 +13,18 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        self.clear()
         self.write(f"Score:{self.score}", align="center",font=("Arial",20,"normal"))
     
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER",align="center",font=("Arial",20,"normal"))
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER",align="center",font=("Arial",20,"normal"))
     
+    def reset(self):
+        if self.score>self.high_score:
+            self.high_score=self.score
+        self.score=0
+
     def increase_Score(self):
         self.score+=1
-        self.clear()
         self.update_scoreboard()
